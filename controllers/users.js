443a8956b6ff8a15011c642f362dc3a569c6b435
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-07-30 03:55:16 +0530

    MongoDB/Mongoose
    adding MongoDB to our project.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,32 @@
+const User = require("../models/user");
+
 module.exports = {
   signUp: async (req, res, next) => {
     // Email & Password
     // req.value.body
     console.log("contents of req.value.body", req.value.body);
     console.log("UsersController.signUp() called!");
+
+    const { email, password } = req.value.body;
+    // Check if there is user with the same email
+
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      return res.status(403).send({ error: "Email is already in use" });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      email: email,
+      password: password,
+    });
+    await newUser.save();
+
+    // Respond with token
+    res.json({ user: "created" });
+
+    //  const email = req.value.body.email;
+    //  const Password = req.value.body.password;
   },
   signIn: async (req, res, next) => {
     // Generate token
